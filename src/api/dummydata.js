4b9b86301d93bc4e2fa3d5ecd12b52ed26e4661c
Seungyun <cherishxyun@gmail.com>
2021-10-25 07:49:44 +0900

    #30 dummydata 11-20

--- src/api/dummydata.js
+++ src/api/dummydata.js
@@ -1,13 +1,13 @@
 const dummydata = [
   {
     id: 1,
-    name: "TAEYEON",
+    name: "Taeyeon",
     birth: 1989,
     image: "../../taeyeon.png",
   },
   {
     id: 2,
-    name: "JENNIFER LAWRENCE",
+    name: "Jennifer Lawrence",
     birth: 1990,
     image: "../../jenniferlawrence.png",
   },
@@ -31,7 +31,7 @@ const dummydata = [
   },
   {
     id: 6,
-    name: "LEE JUNG JAE",
+    name: "Lee Jung-jae",
     birth: 1972,
     image: "LeeJungJae.jpeg",
   },
@@ -43,16 +43,76 @@ const dummydata = [
   },
   {
     id: 9,
-    name: "BLAKE LIVELY",
+    name: "Blake Lively",
     birth: 1987,
     image: "BlakeLively.jpeg",
   },
   {
     id: 10,
-    name: "RYAN REYNOLDS",
+    name: "Ryan Reynolds",
     birth: 1976,
     image: "RyanReynolds.jpeg",
   },
+  {
+    id: 11,
+    name: "Heize",
+    birth: 1991,
+    image: "Heize.jpeg"
+  },
+  {
+    id: 12, 
+    birth: 1996,
+    name: "Jennie",
+    image: "Jennie.jpeg"
+  },
+  {
+    id: 13, 
+    birth: 1979,
+    name: "Im Soo-jung",
+    image: "Im Soo-jung.jpeg"
+  },
+  {
+    id: 14, 
+    birth: 1971,
+    name: "You Hee-yeol",
+    image: "You Hee-yeol.jpeg"
+  },
+  {
+    id: 15, 
+    birth: 1972,
+    name: "You Jae-suk",
+    image: "You Jae-suk.jpeg"
+  },
+  {
+    id: 16, 
+    birth: 1944,
+    name: "O Yeong-su",
+    image: "O Yeong-su.jpeg"
+  },
+  {
+    id: 17, 
+    birth: 1979,
+    name: "Lee Hyo-ri",
+    image: "Lee Hyo-ri.jpeg"
+  },
+  {
+    id: 18,
+    birth: 1990,
+    name: "Emma Watson",
+    image: "Emma Watson.jpeg"
+  },
+  {
+    id: 19,
+    birth: 1985,
+    name: "Amanda Seyfried",
+    image: "Amanda Seyfried.jpeg"
+  },
+  {
+    id: 20,
+    birth: 1962,
+    name: "Tom Cruise",
+    image: "Tom Cruise.jpeg"
+  }
 ];
 
 export default dummydata;
