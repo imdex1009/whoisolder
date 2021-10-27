5956c101e90391c2dec84565182b56148f0f0886
rec.fffixing <imdex1009@gmail.com>
2021-10-28 08:00:48 +0900

    플레이 컴포넌트 리덕스 적용

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,12 +2,11 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Button from "../components/Button";
 
-
 function Main() {
-  const state = useSelector(state => state.auxReducer);
-  const {header} = state;
-  console.log(state)
-  
+  const state = useSelector((state) => state.auxReducer);
+  const { header } = state;
+  console.log(state);
+
   return (
     <section>
       <div className="header">
